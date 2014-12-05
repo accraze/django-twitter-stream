c0eb5a69d8cc1ea8f130387f5da6a2573b28e2c6
accraze <accraze@gmail.com>
2014-12-05 13:11:32 -0700

    user feeds

--- mysite/stream_twitter/views.py
+++ mysite/stream_twitter/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from stream_twitter.models import Follow
 from stream_twitter.models import Tweet
+from django.contrib.auth.models import User
+from stream_django.enrich import Enrich
+from stream_django.feed_manager import feed_manager
+
+
+def profile_feed(request, username=None):
+    enricher = Enrich()
+    user = User.objects.get(username=username)
+    feed = feed_manager.get_user_feed(user.id)
+    activities = feed.get(limit=25)['results']
+    enricher.enrich_activities(activities)
+    context = {
+        'activities': activities
+    }
+    return render(request, 'tweets.html', context)
 
 
 class TweetView(CreateView):
